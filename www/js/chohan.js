08562bb5469f1b2e70778daac68e15fc940cf775
chokobananamaru <akaba7126@gmail.com>
2021-01-13 02:56:14 +0900

    丁半修正

--- www/js/chohan.js
+++ www/js/chohan.js
@@ -1,13 +1,10 @@
 var bet = 0;
+
 function Bet(ibet,obj){
-  var moneybank = Number(localStorage.getItem("moneybank"));
-  moneybank = 100000;
-  localStorage.setItem("moneybank", moneybank);
-    document.getElementById("money").innerText = "お金：" + moneybank + "円";
-  bt1 = document.getElementById("bt1");
-  bt2 = document.getElementById("bt2");
-  bt3 = document.getElementById("bt3");
-  bet = ibet;
+  var bt1 = document.getElementById("bt1");
+  var bt2 = document.getElementById("bt2");
+  var bt3 = document.getElementById("bt3"); 
+  bet = ibet;    
   bt1.disabled = false;
   bt2.disabled = false;
   bt3.disabled = false;
@@ -18,7 +15,10 @@ function Play(chohan){
   var text = "";
     // データを取得
   var moneybank = Number(localStorage.getItem("moneybank"));
-  if(moneybank < bet || bet == 0){
+  if(bet != 1 && bet !=10 && bet != 100){
+    alert("掛け金を選択してください。")
+  }
+  else if(moneybank < bet){
     alert("現金が足りません");
   }
   else{
@@ -28,16 +28,19 @@ function Play(chohan){
     switch(bet){
       case 1:
         for(var i = 0;i<4;i++){
-          gm +=  Math.floor( Math.random() * 3 ) -1;
+          gm +=  Math.floor( Math.random() * 2 ) +1;
         }
+        break;
       case 10:
         for(var i = 0;i<4;i++){
-          gm +=  Math.floor( Math.random() * 31) -15;
+          gm +=  Math.floor( Math.random() * 11) + 5;
         }
+        break;
       case 100:
          for(var i = 0;i<4;i++){
-          gm +=  Math.floor( Math.random() * 301) -150;
+          gm +=  Math.floor( Math.random() * 101) + 50;
         }
+        break;
     } 
     var dicesum = dice1 + dice2;
     if(dicesum % 2 == 0){
@@ -63,5 +66,12 @@ function Play(chohan){
     alert(text);
      localStorage.setItem("moneybank", moneybank);
     document.getElementById("money").innerText = "お金：" + moneybank + "円";
+    bet = 0;
+    var bt1 = document.getElementById("bt1");
+    var bt2 = document.getElementById("bt2");
+    var bt3 = document.getElementById("bt3"); 
+    bt1.disabled = false;
+    bt2.disabled = false;
+    bt3.disabled = false;
   }
 }
