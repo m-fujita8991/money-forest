20d06881056242be50a2f905febefc58cc88944a
teru-410 <keito.1213.tm@gmail.com>
2021-01-13 10:17:54 +0900

    ｑ

--- www/js/off.js
+++ www/js/off.js
@@ -1,17 +1,24 @@
-function onDeviceReady() {
-    var Fetcher = window.BackgroundFetch;
-    var fetchCallback = function() {
-      
-      // バックグラウンド処理
+var applicationKey = "89a435ad7bd29469d2354d179fb078e1865dba3aae9fc730b88390301d4ed2ee";
+
+var clientKey = "756caca44a56dbbf0a60808da7978480274e81b8ebbf5a2720ae05085c1c34af";
+
+// オフライン
+document.addEventListener("offline", onOffline, false);
+function onOffline() {
+    // イベントの処理
+    moneybank += 10;
+    setInterval(function(){
       moneybank += 10;
-      setInterval(function(){
-        moneybank += 10;
-      },6000);
-    }
- 
-    // 関数を登録
-    Fetcher.configure(fetchCallback, failureCallback, {
-        stopOnTerminate: false
-    });
+    },6000);
+    onlineFlag = false;
 }
-document.addEventListener("deviceready", onDeviceReady, false);
+
+// オンライン
+document.addEventListener("online", onOnline, false);
+function onOnline() {
+    // イベントの処理
+    onlineFlag = true;
+}
+
+localStorage.setItem("moneybank", moneybank);
+document.getElementById("money").innerText = "お金：" + moneybank + "円";
