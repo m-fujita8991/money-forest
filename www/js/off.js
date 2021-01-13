f2489df6f3a907e8b43cd644fff262e36d9aa61d
teru-410 <keito.1213.tm@gmail.com>
2021-01-13 13:58:29 +0900

    q

--- www/js/off.js
+++ www/js/off.js
@@ -2,31 +2,24 @@ var applicationKey = "518d2dcf0793d7e878024bfae55edca66fe98d07f37cef22ea14080c51
 
 var clientKey = "0986546d9a7a03b1d7e1c25222ba6694e149405b9b92864d6b263ac8895be0ab";
 
-function money(){
+var moneybank = Number(localStorage.getItem("moneybank"));
+var money = Number(localStorage.getItem("money"));
+var online = false;
 
-  var moneybank = Number(localStorage.getItem("moneybank"));
-  var money = Number(localStorage.getItem("money"));
-  var online = false;
+// オフライン
+document.addEventListener("offline", onOffline, false);
+function onOffline() {
+  // イベントの処理
+  setInterval(function(){
+    moneybank += 10;
+    localStorage.setItem("moneybank", moneybank);
+  },6000);
+  online = false;
+}
 
-  // オフライン
-  document.addEventListener("offline", onOffline, false);
-  function onOffline() {
-      // イベントの処理
-      moneybank += 10;
-      setInterval(function(){
-        moneybank += 10;
-      },6000);
-      online = false;
-  }
-
-  // オンライン
-  document.addEventListener("online", onOnline, false);
-  function onOnline() {
-      // イベントの処理
-      online = true;
-  }
-
-  localStorage.setItem("moneybank", moneybank);
-  document.getElementById("money").innerText = "お金：" + moneybank + "円";
-
+// オンライン
+document.addEventListener("online", onOnline, false);
+function onOnline() {
+  // イベントの処理
+  online = true;
 }
