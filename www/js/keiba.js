649ac0f21e2d6ee52d2051bbb861b8319282c429
chokobananamaru <akaba7126@gmail.com>
2021-01-15 09:40:33 +0900

    ffffffff

--- www/js/keiba.js
+++ www/js/keiba.js
@@ -5,6 +5,9 @@ var clientKey = "0986546d9a7a03b1d7e1c25222ba6694e149405b9b92864d6b263ac8895be0a
 
 document.addEventListener("DOMContentLoaded",function(){
   var fan = new Audio();
+  var moneybank = Number(localStorage.getItem("moneybank"));
+  moneybank += 10000;
+  localStorage.setItem("moneybank",moneybank);
   var rb = -1;
   var rank = [0,1,2,3,4];
   var betmoney = 0;
@@ -80,7 +83,7 @@ document.addEventListener("DOMContentLoaded",function(){
   }
 
   function Val(betmoney){
-    var reg = new RegExp("^[1-9][0-9]{4,}$");
+    var reg = new RegExp("^[5-9][0-9]{3,}$");
     if(!reg.test(betmoney)){
       alert("掛け金を正しく入力してください");
       return true;
