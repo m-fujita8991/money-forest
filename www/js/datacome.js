506ee364ba54c02a96608a577747931dad356753
m-fujita8991 <masasa6k@gmail.com>
2021-01-15 13:56:19 +0900

    pipi

--- www/js/datacome.js
+++ www/js/datacome.js
@@ -1,7 +1,7 @@
 // 名前取得用js
 // APIキー
-var applicationKey = "518d2dcf0793d7e878024bfae55edca66fe98d07f37cef22ea14080c5174b485";
-var clientKey = "0986546d9a7a03b1d7e1c25222ba6694e149405b9b92864d6b263ac8895be0ab";
+var applicationKey = "*****";
+var clientKey = "*****";
 
 // 名前取得
 window.addEventListener('load', (event) => {
--- www/js/gacha.js
+++ www/js/gacha.js
@@ -1,6 +1,6 @@
 // This is a JavaScript file
-var applicationKey = "518d2dcf0793d7e878024bfae55edca66fe98d07f37cef22ea14080c5174b485";
-var clientKey = "0986546d9a7a03b1d7e1c25222ba6694e149405b9b92864d6b263ac8895be0ab";
+var applicationKey = "*****";
+var clientKey = "*****";
 // SDKの初期化
 var ncmb = new NCMB(applicationKey, clientKey);
 
